d1855a8152b7b57dc7fcbe84c3069f2194984de8
Varma Bhupatiraju <varmab@gmail.com>
2019-08-20 18:51:26 +0530

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome'
+import Counter from './Counter'
+import Friends from './Friends'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Welcome name="Varma" password="123456" year={2020}/>
+          <Welcome name="Ram" password="abcdef"/>
+        </header>
+        <Friends/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Welcome extends Component{
+    constructor(){
+        super();
+        this.state={
+            clicks:0
+        }
+    }
+
+    updateClicks=()=>{
+        this.setState({
+            clicks:this.state.clicks+1
+        })
+    }
+
+    render(){
+        return(
+            <h1 onClick={this.updateClicks}>Welcome to {this.props.name} on {this.props.year} - ({this.state.clicks})</h1>
+        )
+    }
+}
+
+Welcome.propTypes={
+    name:PropTypes.string.isRequired,
+    year:PropTypes.number,
+    password(props, propName, component){
+        if(!(propName in props)){
+          return new Error(`missing ${propName}`)
+        }
+        if(props[propName].length < 6){
+          return new Error(`${propName} was too short`)
+        }
+      }
+}
+
+Welcome.defaultProps={
+    year:2020
+}
+
+export default Welcome;
